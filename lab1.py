47bf36c2ac7ebdf0603ad2e77834853c9736bfaf
Sviatoslav Darmohrai <darmohrays@gmail.com>
2018-10-07 17:52:44 +0300

    First commit

---
+++ lab1.py
@@ -0,0 +1,49 @@
+import itertools
+import collections
+
+# #1.1
+# print("Hello world")
+
+# #1.2
+# def function(number1, number2):
+# 	if number1 < 0 or number2 < 0:
+# 		print("Exception")   #!!!!!
+# 		return False
+# 	elif number1%number2==0:
+# 		return True
+
+# print(function(4, 2))
+	
+# #1.3
+
+# def isComposite(n):
+# 	temp = False
+# 	for i in range(2,n):
+# 		if function(n % i):
+# 		   temp = True
+# 		   break
+# 	return temp
+
+# def simpleRange(a, b):
+# 	l = []
+# 	for i in range(a, b):
+# 		if not isComposite(i):
+# 			l.append(i)
+# 	return l	
+
+# print simpleRange(1, 10)
+
+# #1.4
+# arr = ['a', ['c', 1, 3], ['f', 7, [4, '4']], [{'lalala': 111}]]
+
+arr = []
+arr1 = []
+for sublist in arr:
+	for item in sublist:
+		if isinstance(item, list):
+			for i in item:
+				arr1.append(i)
+		else:
+			arr1.append(item)
+print arr1
+	
---
+++ lab2.py
@@ -0,0 +1,65 @@
+import xml.etree.ElementTree as etree
+import re
+from collections import OrderedDict
+from itertools import izip, repeat
+
+#2.1
+def f(n):
+    file = open('a.txt', 'r')
+    output = file.readlines()[0:n]
+    return output
+
+print f(2)
+
+#2.2
+def f1():
+    words = []
+
+    with open("a.txt") as f:
+        words = [word for line in f for word in line.split()]
+    
+    b1 = open('b1.txt', 'w')
+    b2 = open('b2.txt', 'w')
+
+    for i in range(0, len(words)):
+        temp = words[i] + " "
+        if i%2:
+            b1.write(temp.upper())
+        else:
+            b2.write(temp.lower())
+    return words
+
+f1()
+
+#2.3
+def f2(wordsArr):
+    words = etree.Element("words")
+    wordsList = []
+    for word in wordsArr:
+        if word not in wordsList:
+            wordsList.append(word)
+            etree.SubElement(words, "word", name = word).text = str(wordsArr.count(word))
+    tree = etree.ElementTree(words)
+    tree.write("filename.xml")
+    return wordsList
+
+f2(f1())
+
+#2.4
+def f3():
+    with open("a.txt", 'r') as f:
+        wordsList = f.read().replace('\n', '')
+
+    endings = []
+    words = etree.Element("words")
+    temp = re.findall(r'\w\w\w\b', wordsList)
+    endings = list(OrderedDict(izip(temp, repeat(None))))
+    for ending in endings:
+        wordsWithSameEnding = re.findall(re.compile(ending + '\b'), wordsList)
+        etree.SubElement(words, "ending", end = ending)
+        for w in wordsWithSameEnding:
+            etree.SubElement(ending, "word", word = w)
+    tree = etree.ElementTree(words)
+    tree.write("c.xml")
+
+f3()
