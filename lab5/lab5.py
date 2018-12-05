c63cd997973290f0a53df1e0ea9cc5ec853bf86d
Sviatoslav Darmohrai <darmohrays@gmail.com>
2018-12-05 10:27:20 +0200

    Did 5 lab

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,7 +1,7 @@
 import numpy
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 n=207
 m=16
@@ -16,7 +16,17 @@ def find_closest(q, array):
 closest = find_closest(200, array)
 print(closest)
 
-img = Image.open('0_0.jpg').convert("L")
+
+img = Image.open('0_0.jpg')
 arr = numpy.asarray(img)
+
 plt.imshow(arr, cmap='gray')
+plt.show()
+
+image = plt.imread('0_0.jpg')
+arr = numpy.asarray(image)
+
+image1 = ImageEnhance.Contrast(Image.fromarray(numpy.uint8(arr))).enhance(15.0)
+
+plt.imshow(image1)
 plt.show()
